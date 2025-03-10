f85793d163acec9549d88ec906301ee3e5fc2f7c
anonimak <anonimak@gmail.com>
2025-03-11 00:30:47 +0700

    fix:basepath

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,8 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "" : "",
+      base: "", // Important: empty string for user sites
+      assets: "", // Optional: add this if you're having asset path issues
     },
   },
 };
